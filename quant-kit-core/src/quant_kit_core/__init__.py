a7f36a4c7b9c5ee00b6a1f94811f422ef7f81f33
libertininick <libertini.nick@gmail.com>
2022-07-25 20:26:21 -0600

    Add get_rolling_windows

--- quant-kit-core/src/quant_kit_core/__init__.py
+++ quant-kit-core/src/quant_kit_core/__init__.py
@@ -1,6 +1,6 @@
 from os import path
 
-from quant_kit_core.utils import get_timediff
+from quant_kit_core.utils import get_timediff, get_rolling_windows
 
 ROOT_DIR = path.dirname(path.abspath(__file__))
 
@@ -8,5 +8,6 @@ __version__ = "1.0.0.dev0"
 
 __all__ = [
     "ROOT_DIR",
+    "get_rolling_windows",
     "get_timediff",
 ]
--- quant-kit-core/src/quant_kit_core/utils.py
+++ quant-kit-core/src/quant_kit_core/utils.py
@@ -5,10 +5,66 @@ from calendar import isleap
 from datetime import datetime, timedelta
 from typing import List, Tuple, Union
 
+import numpy as np
+from numpy import ndarray
+
 
 __all__ = [
+    "get_rolling_windows",
     "get_timediff",
 ]
+
+
+def get_rolling_windows(arr: ndarray, window_size: int, stride: int = 1) -> ndarray:
+    """Generate a rolling window of input based on a target window and step size
+
+    Parameters
+    ----------
+    arr: ndarray, shape=(N,...)
+        A n-dim array
+    window_size: int
+        Rolling window size.
+    stride: int, optional
+        Window step size.
+        (default = 1)
+
+    Returns
+    -------
+    windows: ndarray, shape=((N - window_size) // stride + 1, window_size, ...)
+        Rolling windows from input.
+
+    Examples
+    --------
+    >>> x = np.arange(10)
+    >>> get_rolling_windows(x, window_size=3)
+    array([
+       [0, 1, 2],
+       [1, 2, 3],
+       [2, 3, 4],
+       [3, 4, 5],
+       [4, 5, 6],
+       [5, 6, 7],
+       [6, 7, 8],
+       [7, 8, 9]
+    ])
+
+    >>> get_rolling_windows(x, window_size=3, stride=2)
+    array([
+       [0, 1, 2],
+       [2, 3, 4],
+       [4, 5, 6],
+       [6, 7, 8]
+    ])
+
+    >>> x = np.random.rand(37,5)
+    >>> windows = get_rolling_windows(x, window_size=6, stride=3)
+    >>> windows.shape
+    (11, 6, 5)
+    """
+    shape = (arr.shape[0] - window_size + 1, window_size) + arr.shape[1:]
+    strides = (arr.strides[0],) + arr.strides
+    rolled = np.lib.stride_tricks.as_strided(arr, shape=shape, strides=strides)
+    return rolled[np.arange(0, shape[0], stride)]
 
 
 def get_timediff(dt1: Union[datetime, str], dt2: Union[datetime, str]) -> float:
